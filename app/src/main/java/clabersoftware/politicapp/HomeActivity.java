cb89f1f06e5266a1ea0cba144c2ee764af17593a
Yann <yann.cla@gmail.com>
2018-10-22 19:13:01 +0200

    Activité parties
    Ajout de l'activité et de l'intent de liaison

--- app/src/main/java/clabersoftware/politicapp/HomeActivity.java
+++ app/src/main/java/clabersoftware/politicapp/HomeActivity.java
@@ -1,7 +1,9 @@
 package clabersoftware.politicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,6 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void showPartiesList(View view) {
+        Intent intent = new Intent(this, PartiesActivity.class);
+        startActivity(intent);
     }
 
 }
