a9c3085c5949850163332feca5c6d621985dbda9
Yann <yann.cla@gmail.com>
2018-10-24 19:29:29 +0200

    Implémentation du changement de langue

--- app/src/main/java/clabersoftware/politicapp/ActionBar.java
+++ app/src/main/java/clabersoftware/politicapp/ActionBar.java
@@ -1,9 +1,16 @@
 package clabersoftware.politicapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Locale;
+
+import clabersoftware.politicapp.Party.AddPartyActivity;
 
 public abstract class ActionBar extends AppCompatActivity {
 
@@ -18,10 +25,50 @@ public abstract class ActionBar extends AppCompatActivity {
                 Intent intent = new Intent(this, HomeActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.action_language:
+                changeLanguage();
+                    return true;
             default:
                 return false;
         }
 
+    }
+
+    private void changeLanguage() {
+        String [] languages  = {"Francais", "English"};
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setSingleChoiceItems(languages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+            switch(i){
+                case 0:
+                    setLanguage("fr");
+                    reaload();
+                    break;
+                case 1:
+                    setLanguage("en");
+                    reaload();
+                    break;
+                }
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void reaload() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void setLanguage(String language) {
+        Locale myLocale = new Locale(language);//Set Selected Locale
+        Locale.setDefault(myLocale);//set new locale as default
+        Configuration config = new Configuration();//get Configuration
+        config.locale = myLocale;//set config locale as selected locale
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
     }
 
 }
