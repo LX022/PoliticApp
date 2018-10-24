54861fdb5bca3b4cb69c3207545b73cbb2256be9
Yann <yann.cla@gmail.com>
2018-10-24 11:21:41 +0200

    ajout des id activity_add_partiy.xml

--- app/src/main/java/clabersoftware/politicapp/Party/AddPartyActivity.java
+++ app/src/main/java/clabersoftware/politicapp/Party/AddPartyActivity.java
@@ -1,7 +1,9 @@
 package clabersoftware.politicapp.Party;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import clabersoftware.politicapp.R;
 
@@ -11,5 +13,10 @@ public class AddPartyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_party);
+    }
+
+    public void add(View view) {
+        Intent intent = new Intent(this, PartiesActivity.class);
+        startActivity(intent);
     }
 }
