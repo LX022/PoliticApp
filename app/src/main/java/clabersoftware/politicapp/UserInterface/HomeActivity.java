b4a9f4ebca60da654b2beda5faf2cdf84e0d53fc
Yann <yann.cla@gmail.com>
2018-11-16 14:49:27 +0100

    toVote

--- app/src/main/java/clabersoftware/politicapp/UserInterface/HomeActivity.java
+++ app/src/main/java/clabersoftware/politicapp/UserInterface/HomeActivity.java
@@ -8,6 +8,7 @@ import clabersoftware.politicapp.R;
 import clabersoftware.politicapp.UserInterface.Party.PartiesListActivity;
 import clabersoftware.politicapp.UserInterface.Politician.PoliticianListActivity;
 import clabersoftware.politicapp.UserInterface.Politician.PoliticiansActivity;
+import clabersoftware.politicapp.UserInterface.VotingObject.VoteListActivity;
 import clabersoftware.politicapp.UserInterface.VotingObject.VotingObjectsListActivity;
 
 public class HomeActivity extends BaseActivity {
@@ -29,6 +30,11 @@ public class HomeActivity extends BaseActivity {
 
     public void showVotingObjectList(View view) {
         Intent intent = new Intent(this, VotingObjectsListActivity.class);
+        startActivity(intent);
+    }
+
+    public void vote(View view) {
+        Intent intent = new Intent(this, VoteListActivity.class);
         startActivity(intent);
     }
 
