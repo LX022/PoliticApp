673be382ad6f30ae58fc095314c84698ec980ed9
Yann <yann.cla@gmail.com>
2018-11-17 15:32:19 +0100

    Results

--- app/src/main/java/clabersoftware/politicapp/UserInterface/HomeActivity.java
+++ app/src/main/java/clabersoftware/politicapp/UserInterface/HomeActivity.java
@@ -9,6 +9,7 @@ import clabersoftware.politicapp.R;
 import clabersoftware.politicapp.UserInterface.Party.PartiesListActivity;
 import clabersoftware.politicapp.UserInterface.Politician.PoliticianListActivity;
 import clabersoftware.politicapp.UserInterface.Politician.PoliticiansActivity;
+import clabersoftware.politicapp.UserInterface.VotingObject.ResultsListActivity;
 import clabersoftware.politicapp.UserInterface.VotingObject.VoteListActivity;
 import clabersoftware.politicapp.UserInterface.VotingObject.VotingObjectsListActivity;
 
@@ -37,6 +38,11 @@ public class HomeActivity extends BaseActivity {
 
     public void vote(View view) {
         Intent intent = new Intent(this, VoteListActivity.class);
+        startActivity(intent);
+    }
+
+    public void results(View view) {
+        Intent intent = new Intent(this, ResultsListActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/clabersoftware/politicapp/UserInterface/VotingObject/ResultsListActivity.java
@@ -0,0 +1,68 @@
+package clabersoftware.politicapp.UserInterface.VotingObject;
+
+import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import clabersoftware.politicapp.DataBase.AppDatabase;
+import clabersoftware.politicapp.DataBase.Entity.VotingObjectEntity;
+import clabersoftware.politicapp.DataBase.async.VotingObjectAsync;
+import clabersoftware.politicapp.R;
+import clabersoftware.politicapp.UserInterface.BaseActivity;
+
+public class ResultsListActivity extends BaseActivity {
+
+    ListView theListView;
+    Intent myIntent;
+    private AppDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        db = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DATABASE_NAME).build();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_list);
+
+        myIntent = new Intent(this, EditVotingObjectActivity.class);
+
+        theListView = (ListView) findViewById(R.id.VotingObjectsListView);
+
+        List<VotingObjectEntity> datas = genererVotingObjects();
+
+        ArrayAdapter<VotingObjectEntity> VotingObjectsAdapter = new ArrayAdapter<VotingObjectEntity>(this, android.R.layout.simple_list_item_1, datas);
+
+        theListView.setAdapter(VotingObjectsAdapter);
+
+        theListView.setOnItemClickListener( listClick );
+    }
+
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener () {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            VotingObjectEntity itemValue = (VotingObjectEntity) theListView.getItemAtPosition( position );
+            itemValue.getIdVotingObject();
+            myIntent.putExtra("VOTINGOBJECT_SELECTED", itemValue.getIdVotingObject());
+            startActivity(myIntent);
+        }
+    };
+
+    private List<VotingObjectEntity> genererVotingObjects(){
+        List<VotingObjectEntity> votingObjects = new ArrayList<>();
+        try {
+            votingObjects = (ArrayList) new VotingObjectAsync(db,   "getAll", 0).execute().get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return votingObjects;
+    }
+}
