8c3f98d9f8be9e6a3670a3896635d9417fb6aeb2
Alex <berclazalexandre@gmail.com>
2018-11-17 18:46:56 +0100

    Update LoginActivity.java

--- app/src/main/java/clabersoftware/politicapp/UserInterface/Login/LoginActivity.java
+++ app/src/main/java/clabersoftware/politicapp/UserInterface/Login/LoginActivity.java
@@ -16,10 +16,16 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import clabersoftware.politicapp.DataBase.AppDatabase;
+import clabersoftware.politicapp.DataBase.Entity.PartyEntity;
 import clabersoftware.politicapp.DataBase.Entity.PoliticianEntity;
+import clabersoftware.politicapp.DataBase.Entity.VotingLineEntity;
+import clabersoftware.politicapp.DataBase.Entity.VotingObjectEntity;
 import clabersoftware.politicapp.DataBase.GlobalData;
 import clabersoftware.politicapp.DataBase.async.PartyAsync;
 import clabersoftware.politicapp.DataBase.async.PoliticianAsync;
+import clabersoftware.politicapp.DataBase.async.VotingLineAsync;
+import clabersoftware.politicapp.DataBase.async.VotingObjectAsync;
+import clabersoftware.politicapp.UserInterface.BaseActivity;
 import clabersoftware.politicapp.UserInterface.HomeActivity;
 import clabersoftware.politicapp.R;
 
@@ -32,7 +38,18 @@ public class LoginActivity extends AppCompatActivity {
         db = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DATABASE_NAME).build();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /*
+        * Si base vide alors on génère des données de base*/
+        List<PoliticianEntity> allPolitician = generatePolitician();
+        System.out.println(allPolitician.toString());
+        if (allPolitician.isEmpty()){
+            generateData();
+
+        }
     }
+
+
 
     //Boutton Sign up
     public void signUp(View view) {
@@ -146,6 +163,79 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return id;
+    }
+
+    //liste des politiciens
+    private List<PoliticianEntity> generatePolitician(){
+        List<PoliticianEntity> politicians = new ArrayList<>();
+        try {
+            politicians = (ArrayList) new PoliticianAsync(db, "getAll", 0).execute().get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return politicians;
+    }
+
+    private void generateData() {
+        new PoliticianAsync(db, "deleteAll", 1).execute();
+        new VotingLineAsync(db, "deleteAll", 1).execute();
+        new PartyAsync(db, "deleteAll", 1).execute();
+        new VotingObjectAsync(db, "deleteAll", 1).execute();
+
+        System.out.println("Delete ALL Ok");
+
+        PartyEntity p1 = new PartyEntity("Red", "PS", "Parti Socialiste");
+        PartyEntity p2 = new PartyEntity("Blue", "PLR", "Parti Libéral Radical");
+        PartyEntity p3 = new PartyEntity("White", "sans parti", "Sans Parti");
+
+        new PartyAsync(db,"add",p1).execute();
+        new PartyAsync(db,"add",p2).execute();
+        new PartyAsync(db,"add",p3).execute();
+
+        //String firstName, String lastName,String password, long fkParty, String login
+        PoliticianEntity po1 = new PoliticianEntity("Grégoire","Clavien","1234",new Long(1),"g.clavien");
+        PoliticianEntity po2 = new PoliticianEntity("Paul","Berclaz","1234",new Long(3),"p.berclaz");
+        PoliticianEntity po3 = new PoliticianEntity("Manu","Amoos","1234",new Long(1),"m.amoos");
+        PoliticianEntity po4 = new PoliticianEntity("Alexandre","Berclaz","1234",new Long(1),"a.berclaz");
+        PoliticianEntity po5 = new PoliticianEntity("Yann","Clavien","1234",new Long(1),"y.clavien");
+        PoliticianEntity po6 = new PoliticianEntity("Monique","Vuignier","1234",new Long(1),"m.vuignier");
+        PoliticianEntity po7 = new PoliticianEntity("Noé","Zufferey","1234",new Long(1),"n.zufferey");
+
+        new PoliticianAsync(db,"add",po1).execute();
+        new PoliticianAsync(db,"add",po2).execute();
+        new PoliticianAsync(db,"add",po3).execute();
+        new PoliticianAsync(db,"add",po4).execute();
+        new PoliticianAsync(db,"add",po5).execute();
+        new PoliticianAsync(db,"add",po6).execute();
+        new PoliticianAsync(db,"add",po7).execute();
+
+        //String entitled, String details, String date
+        VotingObjectEntity vo1 = new VotingObjectEntity("Lex Weber", "Votation concernant les résidences secondaires au niveau suisse", "01.01.2018");
+        VotingObjectEntity vo2 = new VotingObjectEntity("LAT", "Votation sur l'aménagement du téritoire", "01.02.2018");
+
+        new VotingObjectAsync(db,"add",vo1).execute();
+        new VotingObjectAsync(db,"add",vo2).execute();
+
+        //String vote, long fkPolitician, long fkVotingObject
+        VotingLineEntity vl1 = new VotingLineEntity("Yes",new Long(1),new Long(1));
+        VotingLineEntity vl2 = new VotingLineEntity("Yes",new Long(2),new Long(1));
+        VotingLineEntity vl3 = new VotingLineEntity("No",new Long(3),new Long(1));
+        VotingLineEntity vl4 = new VotingLineEntity("No",new Long(4),new Long(1));
+        VotingLineEntity vl5 = new VotingLineEntity("No",new Long(5),new Long(1));
+        VotingLineEntity vl6 = new VotingLineEntity("Blank",new Long(6),new Long(1));
+        VotingLineEntity vl7 = new VotingLineEntity("No",new Long(7),new Long(1));
+
+        new VotingLineAsync(db,"add",vl1).execute();
+        new VotingLineAsync(db,"add",vl2).execute();
+        new VotingLineAsync(db,"add",vl1).execute();
+        new VotingLineAsync(db,"add",vl3).execute();
+        new VotingLineAsync(db,"add",vl5).execute();
+        new VotingLineAsync(db,"add",vl6).execute();
+        new VotingLineAsync(db,"add",vl7).execute();
+
     }
 
 }
